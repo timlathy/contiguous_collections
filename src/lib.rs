638714e95cce313e3bb601f7185aac1dca5e505d
timlathy <tim.labushev@gmail.com>
2024-09-15 11:56:52 +0200

    OrdVec: accept key extraction function as a generic parameter

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! A library of collections backed by flat contiguous arrays.
 
 pub use array2::Array2;
-pub use ordvec::{OrdVec, OrdVecKey};
+pub use ordvec::{OrdVec, OrdVecKey, OrdVecKeyFst};
 
 mod array2;
 mod ordvec;
--- src/ordvec.rs
+++ src/ordvec.rs
@@ -1,42 +1,101 @@
-use std::{fmt, fmt::Debug};
+use std::marker::PhantomData;
 
-/// Ordered [`Vec`] intended for fast lookup of items by key.
-pub struct OrdVec<T: OrdVecKey>(Vec<T>);
+/// Ordered [`Vec<T>`] intended for fast lookup of items by key.
+///
+/// The key is typically stored inside `T` and extracted with the key function `K`.
+/// (Different key functions may be created for the same data type.)
+///
+/// # Examples
+///
+/// ```
+/// # use contiguous_collections::{OrdVec, OrdVecKey};
+/// #[derive(Debug, Clone, PartialEq)]
+/// struct User { uid: usize, name: String, zip: String };
+///
+/// struct UidKey;
+/// impl OrdVecKey<User> for UidKey { type Key = usize; fn get_key(u: &User) -> &usize { return &u.uid; } }
+/// struct ZipKey;
+/// impl OrdVecKey<User> for ZipKey { type Key = str; fn get_key(u: &User) -> &str { return &u.zip; } }
+///
+/// let users = [
+///     User { uid: 1, name: "Maya".into(), zip: "10030".into() },
+///     User { uid: 0, name: "Ben".into(), zip: "11030".into() },
+///     User { uid: 2, name: "Ariel".into(), zip: "11000".into() },
+/// ];
+/// let by_uid = users.iter().cloned().collect::<OrdVec<User, UidKey>>();
+/// assert_eq!(Some(&users[0]), by_uid.get_by_key(&1));
+/// let by_zip = users.iter().cloned().collect::<OrdVec<User, ZipKey>>();
+/// assert_eq!(Some(&users[0]), by_zip.get_by_key("10030"));
+/// ```
+pub struct OrdVec<T, K: OrdVecKey<T>>(Vec<T>, PhantomData<K>);
 
-pub trait OrdVecKey {
-    type Key: Ord;
-    fn get_key(item: &Self) -> Self::Key;
+/// Defines a key extraction function for type `T`.
+/// If the key is not stored as part of the data, use a tuple of (key, data) as `T` and [`OrdVecKeyFst`] as `K`.
+pub trait OrdVecKey<T> {
+    type Key: Ord + ?Sized;
+    fn get_key(item: &T) -> &Self::Key;
 }
 
-impl<K: OrdVecKey, V> OrdVecKey for (K, V) {
-    type Key = K::Key;
+/// Provides a key extraction function that returns the first element of a two-element tuple.
+pub struct OrdVecKeyFst;
+
+impl<K: Ord, D> OrdVecKey<(K, D)> for OrdVecKeyFst {
+    type Key = K;
     #[inline(always)]
-    fn get_key(item: &Self) -> Self::Key {
-        OrdVecKey::get_key(&item.0)
+    fn get_key(item: &(K, D)) -> &Self::Key {
+        &item.0
     }
 }
 
-impl<T: OrdVecKey> OrdVec<T> {
+impl<T, K: OrdVecKey<T>> OrdVec<T, K> {
+    /// Creates an empty [`OrdVec`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use contiguous_collections::{OrdVec, OrdVecKey, OrdVecKeyFst};
+    /// let v: OrdVec<(u32, String), OrdVecKeyFst> = OrdVec::new();
+    /// assert_eq!(0, v.len());
+    /// ```
     pub const fn new() -> Self {
-        OrdVec(Vec::new())
+        OrdVec(Vec::new(), PhantomData)
     }
 
-    pub fn from_unsorted(mut vec: Vec<T>) -> Self {
-        vec.sort_unstable_by_key(<T as OrdVecKey>::get_key);
+    /// Creates an [`OrdVec`] by taking ownership of the given vector
+    /// and sorting it according to the key extraction function.
+    ///
+    /// Prefer this method if you want to be explicit about the
+    /// underlying operations. Otherwise, use the more consice
+    /// [`From<Vec<T>>`](struct.OrdVec.html#impl-From<Vec<T,+Global>>-for-OrdVec<T,+K>).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use contiguous_collections::{OrdVec, OrdVecKey, OrdVecKeyFst};
+    /// let v = vec![(1, "B"), (0, "A"), (3, "D"), (2, "C")];
+    /// let ov: OrdVec<_, OrdVecKeyFst> = OrdVec::new_from_unsorted(v);
+    /// assert_eq!([(0, "A"), (1, "B"), (2, "C"), (3, "D")], ov[..]);
+    /// ```
+    pub fn new_from_unsorted(mut vec: Vec<T>) -> Self {
+        vec.sort_unstable_by(|a, b| K::get_key(a).cmp(K::get_key(b)));
         assert!(
             vec.windows(2)
-                .all(|pair| <T as OrdVecKey>::get_key(&pair[0])
-                    != <T as OrdVecKey>::get_key(&pair[1])),
+                .all(|pair| K::get_key(&pair[0]) != K::get_key(&pair[1])),
             "OrdVec must not contain duplicate keys"
         );
-        OrdVec(vec)
+        OrdVec(vec, PhantomData)
+    }
+
+    /// Returns the number of elements in [`OrdVec`].
+    pub fn len(&self) -> usize {
+        self.0.len()
     }
 
     pub fn insert(&mut self, item: T) {
         let insert_idx = if let Some(last_item) = self.0.last() {
-            let k = <T as OrdVecKey>::get_key(&item);
-            if k <= <T as OrdVecKey>::get_key(&last_item) {
-                match self.0.binary_search_by_key(&k, <T as OrdVecKey>::get_key) {
+            let k = K::get_key(&item);
+            if k <= K::get_key(&last_item) {
+                match self.0.binary_search_by_key(&k, K::get_key) {
                     Ok(_) => panic!("Cannot insert an item with a duplicate key"),
                     Err(i) => i,
                 }
@@ -49,27 +108,27 @@ impl<T: OrdVecKey> OrdVec<T> {
         self.0.insert(insert_idx, item);
     }
 
-    pub fn get_by_key(&self, k: &<T as OrdVecKey>::Key) -> Option<&T> {
+    pub fn get_by_key(&self, k: &<K as OrdVecKey<T>>::Key) -> Option<&T> {
         self.0
-            .binary_search_by_key(k, <T as OrdVecKey>::get_key)
+            .binary_search_by_key(&k, K::get_key)
             .ok()
             .map(|i| &self.0[i])
     }
 
-    pub fn get_mut_by_key(&mut self, k: &<T as OrdVecKey>::Key) -> Option<&mut T> {
+    pub fn get_mut_by_key(&mut self, k: &<K as OrdVecKey<T>>::Key) -> Option<&mut T> {
         self.0
-            .binary_search_by_key(k, <T as OrdVecKey>::get_key)
+            .binary_search_by_key(&k, K::get_key)
             .ok()
             .map(|i| &mut self.0[i])
     }
 
-    // pub fn get_index_by_key(&self, k: &<T as OrdVecKey>::Key) -> Option<usize> {
-    //     self.0.binary_search_by_key(k, <T as OrdVecKey>::get_key).ok()
+    // pub fn get_index_by_key(&self, k: &<K as OrdVecKey<T>>::Key) -> Option<usize> {
+    //     self.0.binary_search_by_key(k, K::get_key).ok()
     // }
 
-    pub fn remove_by_key(&mut self, k: &<T as OrdVecKey>::Key) -> Option<T> {
+    pub fn remove_by_key(&mut self, k: &<K as OrdVecKey<T>>::Key) -> Option<T> {
         self.0
-            .binary_search_by_key(k, <T as OrdVecKey>::get_key)
+            .binary_search_by_key(&k, K::get_key)
             .ok()
             .map(|i| self.0.remove(i))
     }
@@ -78,34 +137,25 @@ impl<T: OrdVecKey> OrdVec<T> {
     /// * Replace the element with the new value if the function returns Some(T),
     /// * Remove the element if the function returns None.
     ///
-    /// The order of iteration may not follow the order of keys. Compare to [`Vec::retain_mut`].
+    /// The order of iteration may not follow the order of keys. Compare to
+    /// [`Vec::retain_mut`](https://doc.rust-lang.org/std/vec/struct.Vec.html#method.retain_mut).
     ///
     /// # Examples
     ///
     /// ```
-    /// # use contiguous_collections::{OrdVec, OrdVecKey};
-    /// # fn main() {
-    /// # #[derive(Debug, PartialEq)]
-    /// # struct K(u32);
-    /// impl OrdVecKey for K {
-    ///   type Key = u32;
-    ///   fn get_key(item: &Self) -> Self::Key { item.0 }
-    /// }
-    /// let mut lv = OrdVec::from_unsorted(vec![
-    ///     (K(0), "0"), (K(1), "1"), (K(2), "2"), (K(3), "3"),
-    ///     (K(4), "4"), (K(5), "5"), (K(6), "6"), (K(7), "7")
-    /// ]);
+    /// # use contiguous_collections::{OrdVec, OrdVecKey, OrdVecKeyFst};
+    /// let mut ov: OrdVec<_, OrdVecKeyFst> = vec![
+    ///     (0, "0"), (1, "1"), (2, "2"), (3, "3"),
+    ///     (4, "4"), (5, "5"), (6, "6"), (7, "7")
+    /// ].into();
     /// let mut retain_map_order = Vec::new();
-    /// lv.retain_map(|(K(i), v)| {
-    ///     retain_map_order.push(i);
-    ///     if i % 2 == 0 { Some((K(6 - i), v)) } else { None }
+    /// ov.retain_map(|(k, v)| {
+    ///     retain_map_order.push(k);
+    ///     if k % 2 == 0 { Some((6 - k, v)) } else { None }
     /// });
-    /// assert_eq!(&lv[..], &[(K(0), "6"), (K(2), "4"), (K(4), "2"), (K(6), "0")]);
-    /// assert_eq!(&retain_map_order[..], &[0, 1, 7, 6, 2, 3, 5, 4]);
-    /// # }
+    /// assert_eq!(ov[..], [(0, "6"), (2, "4"), (4, "2"), (6, "0")]);
+    /// assert_eq!(retain_map_order[..], [0, 1, 7, 6, 2, 3, 5, 4]);
     /// ```
-    ///
-    /// [`Vec::retain_mut`]: https://doc.rust-lang.org/std/vec/struct.Vec.html#method.retain_mut
     pub fn retain_map(&mut self, mut f: impl FnMut(T) -> Option<T>) {
         let mut i = 0;
         while i < self.0.len() {
@@ -119,60 +169,86 @@ impl<T: OrdVecKey> OrdVec<T> {
                 i += 1;
             }
         }
-        self.0.sort_unstable_by_key(<T as OrdVecKey>::get_key);
+        self.0
+            .sort_unstable_by(|a, b| K::get_key(a).cmp(K::get_key(b)));
     }
 }
 
-impl<T: OrdVecKey> std::ops::Deref for OrdVec<T> {
+/// Creates an [`OrdVec`] by taking ownership of the given vector
+/// and sorting it according to the key extraction function.
+///
+/// Identical to [`new_from_unsorted`](struct.OrdVec.html#method.new_from_unsorted).
+///
+/// # Examples
+///
+/// ```
+/// # use contiguous_collections::{OrdVec, OrdVecKey, OrdVecKeyFst};
+/// let v = vec![(1, "B"), (0, "A"), (3, "D"), (2, "C")];
+/// let ov: OrdVec<_, OrdVecKeyFst> = v.into();
+/// assert_eq!([(0, "A"), (1, "B"), (2, "C"), (3, "D")], ov[..]);
+/// ```
+impl<T, K: OrdVecKey<T>> From<Vec<T>> for OrdVec<T, K> {
+    fn from(value: Vec<T>) -> Self {
+        Self::new_from_unsorted(value)
+    }
+}
+
+/// Creates an [`OrdVec`] from an iterator.
+///
+/// # Examples
+///
+/// ```
+/// # use contiguous_collections::{OrdVec, OrdVecKey, OrdVecKeyFst};
+/// let iter = [(1, "B"), (0, "A"), (3, "D"), (2, "C")].into_iter();
+/// let ov = iter.collect::<OrdVec<_, OrdVecKeyFst>>();
+/// assert_eq!([(0, "A"), (1, "B"), (2, "C"), (3, "D")], ov[..]);
+/// ```
+impl<T, K: OrdVecKey<T>> FromIterator<T> for OrdVec<T, K> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        Self::new_from_unsorted(Vec::from_iter(iter))
+    }
+}
+
+/// Returns a slice of the underlying data, which is guaranteed
+/// to be ordered according to the key extraction function.
+impl<T, K: OrdVecKey<T>> std::ops::Deref for OrdVec<T, K> {
     type Target = [T];
     fn deref(&self) -> &[T] {
         &self.0
     }
 }
 
-impl<T: OrdVecKey> From<Vec<T>> for OrdVec<T> {
-    fn from(value: Vec<T>) -> Self {
-        Self::from_unsorted(value)
-    }
-}
-
-impl<T: OrdVecKey> FromIterator<T> for OrdVec<T> {
-    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-        Self::from_unsorted(Vec::from_iter(iter))
-    }
-}
-
-impl<T: OrdVecKey + Clone> Clone for OrdVec<T> {
+impl<T: Clone, K: OrdVecKey<T>> Clone for OrdVec<T, K> {
     fn clone(&self) -> Self {
-        OrdVec(self.0.clone())
+        OrdVec(self.0.clone(), PhantomData)
     }
 }
 
-impl<T: OrdVecKey + Eq> Eq for OrdVec<T> {}
+impl<T: Eq, K: OrdVecKey<T>> Eq for OrdVec<T, K> {}
 
-impl<T: OrdVecKey + PartialEq> PartialEq for OrdVec<T> {
+impl<T: PartialEq, K: OrdVecKey<T>> PartialEq for OrdVec<T, K> {
     fn eq(&self, other: &Self) -> bool {
         self[..] == other[..]
     }
 }
 
-impl<T: OrdVecKey + Debug> Debug for OrdVec<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        fmt::Debug::fmt(&self, f)
+impl<T: std::fmt::Debug, K: OrdVecKey<T>> std::fmt::Debug for OrdVec<T, K> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Debug::fmt(&self, f)
     }
 }
 
 #[cfg(feature = "serde")]
-impl<T: OrdVecKey + serde::Serialize> serde::Serialize for OrdVec<T> {
+impl<T: serde::Serialize, K: OrdVecKey<T>> serde::Serialize for OrdVec<T, K> {
     fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
         self.0.serialize(serializer)
     }
 }
 
 #[cfg(feature = "serde")]
-impl<'de, T: OrdVecKey + serde::Deserialize<'de>> serde::Deserialize<'de> for OrdVec<T> {
+impl<'de, T: serde::Deserialize<'de>, K: OrdVecKey<T>> serde::Deserialize<'de> for OrdVec<T, K> {
     fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
         let vec = Vec::deserialize(deserializer)?;
-        Ok(OrdVec::from_unsorted(vec))
+        Ok(OrdVec::new_from_unsorted(vec))
     }
 }
